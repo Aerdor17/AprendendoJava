9f4326a1f2d79b7b6a619078136b82be46bd98b7
Lucas <lebbeninca@gmail.com>
2019-08-13 14:01:22 -0300

    Herança e polimorfismo
    Herança e polimorfismo com class Conta e duas classes filhas: ContaCorrente e ContaPoupanca

---
+++ banconbit-heradado-conta/src/ContaCorrente.java
@@ -0,0 +1,15 @@
+
+public class ContaCorrente extends Conta {
+
+	public ContaCorrente(int agencia, int numero) { // E ASSIM QUE EU CHAMO UM CONSTRUTOR ESPECIFICO DA SUPERCLASS PARA
+													// A CLASS FILHA
+		super(agencia, numero);
+	}
+
+	@Override
+	public boolean saca(double valor) { // GAMBI
+		double valorSaca = valor + 0.20;
+		return super.saca(valorSaca);
+	}
+
+}
