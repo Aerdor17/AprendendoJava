6b734f42c97c1cdfaf289ad653385111cdb630d5
Lucas <lebbeninca@gmail.com>
2019-08-14 10:58:11 -0300

    praticando abstract

--- bancobit-herdado/src/ControleBonificacao.java
+++ bancobit-herdado/src/ControleBonificacao.java
@@ -5,7 +5,7 @@ public class ControleBonificacao {
 
 	public void registra(Funcionario f) { // TODOS USAM O METODO BONIFICA POREM CADA UM RECEBE UMA BONIFICACAO DIFERENTE
 		double bonifica = f.getBonificacao();
-		this.somabonifica = this.somabonifica + bonifica;
+		this.somabonifica += bonifica;
 	}
 
 	public double getSomabonifica() {
--- banconbit-heradado-conta/src/ContaCorrente.java
+++ banconbit-heradado-conta/src/ContaCorrente.java
@@ -12,4 +12,10 @@ public class ContaCorrente extends Conta {
 		return super.saca(valorSaca);
 	}
 
+	@Override
+	public void deposita(double valor) {
+		this.saldo += valor;
+		
+	}
+
 }
