3b9cf7daff4a1e4317999a4c7b501356f7a4bf85
Lucas <lebbeninca@gmail.com>
2019-08-22 10:39:20 -0300

    Exceptions
    catch(exception 1 | exception 2){
    }

--- java-pilha/src/Fluxo.java
+++ java-pilha/src/Fluxo.java
@@ -2,27 +2,30 @@ public class Fluxo {
 
 	public static void main(String[] args) {
 		System.out.println("Ini do main");
-		metodo1();
-		System.out.println("Fim do main");
+		try {// TRATAMENTO DE EXCEPTIONS
+			metodo1();
+			System.out.println("Fim do main");
+		} catch (ArithmeticException | NullPointerException ex) { // UM CATCH PARA DUAS EXCEPTIONS
+			String msg = ex.getMessage(); //PEGANDO A MENSAGEM DA EXCEPTION
+			System.out.println("ArithmeticException" + msg);
+			ex.printStackTrace(); // PEGANDO O RASTRO DA EXCEPTION
+		}
 	}
 
 	private static void metodo1() {
 		System.out.println("Ini do metodo1");
-		try { // TRATAMENTO DE EXCEPTIONS
-			metodo2();
-			System.out.println("Fim do metodo1");
-		} catch (ArithmeticException ex) {
-			System.out.println("ArithmeticException");
-		}
+		metodo2();
+		System.out.println("Fim do metodo1");
+
 	}
 
 	private static void metodo2() {
 		System.out.println("Ini do metodo2");
 		for (int i = 1; i <= 5; i++) {
 			System.out.println(i);
-
-			int a = i / 0;
-
+			//int a = i / 0;
+			Conta c = null;
+			c.deposita();
 		}
 		System.out.println("Fim do metodo2");
 	}
