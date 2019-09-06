3d366d940a8e3c1eb7599818f38ed323e1369a6d
Lucas <lebbeninca@gmail.com>
2019-09-06 10:58:57 -0300

    aprendendo a lidar com Strings

--- bytebank-herdado-conta/src/br/com/bytebank/banco/test/TesteString.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/test/TesteString.java
@@ -1,18 +1,36 @@
 package br.com.bytebank.banco.test;
 
 public class TesteString {
-	
+
 	public static void main(String[] args) {
+
+		String nome = "Alura"; // object literal
 		
-		String nome = "alura"; // object literal
-		//String outro = new String ("alura"); // má pratica 
+	String vazio = "alu";
+//		String outroVazio = vazio.trim(); // faz com que o espassos não sejam considerados
+//		System.out.println(outroVazio.isEmpty()); // diz se o espaço esta vazio ou não
+//		System.out.println(vazio.contains("alu")); // verifica se alu esta dentro da string
 		
-		//String outra = nome.replace("A", "a"); // muda o A para a
-		
-		String outra = nome.toLowerCase();
-		//String outra = nome.toUpperCase();
+//		for(int i=0;i <nome.length(); i++) {
+//		System.out.println(nome.charAt(i)); // pega o caracter na posição que o for passa 
+//		}
 				
-		System.out.println(outra);
+		//String outra = nome.toLowerCase(); // tudo letra minuscula
 		
+		// String outro = new String ("alura"); // má pratica
+
+		// String outra = nome.replace("A", "a"); // muda o A para a
+		
+		// String outra = nome.toUpperCase(); //tudo letra maiuscula
+
+		// String outra = nome.replace(b, d); // troca uma letra pela outra
+		
+		// char c = nome.charAt(0); // pega a letra que esta nessa posição
+		
+		//String sub = nome.substring(1);
+		//System.out.println(sub); // cria uma substring apartir da string principal
+		
+		//System.out.println(outra);
+
 	}
 }
