d858fa40bab3bbfbc3dd67b7b1f0ed5154f97cd4
Lucas <lebbeninca@gmail.com>
2019-09-10 09:24:58 -0300

    Aprendendo arrays
    Curso de java no alura parte 06: entendendo java.io

--- bytebank-herdado-conta/src/br/com/bytebank/banco/test/Teste.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/test/Teste.java
@@ -7,13 +7,28 @@ import br.com.bytebank.banco.modelo.ContaPoupanca;
 public class Teste {
 	
 	public static void main(String[] args) {
+//		// meu primeiro array
+	int[] idades = new int[5];
+//		
+//		int idade1 = 29;
+//		int idade2 = 39;
+//		int idade3 = 49;
+//		
+//		int idade4 = idades[4];
+//		
+//		System.out.println(idade4);
+//		
+//		System.out.println(idades.length); // mostra o tamanho do array
 		
-		Object cc = new ContaCorrente(22, 33);
-		Object cp = new ContaPoupanca(11, 55);
-		Object cliente = new Cliente();
+		for(int i = 0; i< idades.length; i++) {
+			idades [i] = i * i;
+			System.out.println(idades[i]);
+		}
 		
-		System.out.println(cc);
-		System.out.println(cp);
+		
+		
+		
+		
 		
 	}
 
