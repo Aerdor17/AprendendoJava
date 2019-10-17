69f88549906f7eaec6fd9f7f9ca5f0dabc615169
Lucas <lebbeninca@gmail.com>
2019-10-17 11:11:57 -0300

    lendo uma linha
    lendo e imprimindo uma linha do arquivo.txt lorem

--- java-io/src/br/com/alura/java/io/teste/TesteLeitura.java
+++ java-io/src/br/com/alura/java/io/teste/TesteLeitura.java
@@ -1,17 +1,24 @@
 package br.com.alura.java.io.teste;
 
+import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class TesteLeitura {
 	
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) throws IOException {
 		
 		//Fluxo de entrada com Arquivo
-		FileInputStream fis = new FileInputStream("lorem.txt");// estabelecia a entrada de um arquivo com o inputStream
-		InputStreamReader isr = new InputStreamReader(fis); // InputStreamReader sabe ler os bits do arquivo e transformar em character
+		FileInputStream fis = new FileInputStream("lorem.txt"); // criando um fluxo concreto com dados binários
+		InputStreamReader isr = new InputStreamReader(fis); // melhorando os dados binários transformandoos em caracteres
+		BufferedReader br = new BufferedReader(isr); // juntando os caractertes em linhas
 		
+		String linha = br.readLine(); // lé uma linha
+		System.out.println(linha);
+		
+		br.close();
 		
 	}
 
