5b55266e25ba98c601ea9d294805571f2a5563a0
Lucas <lebbeninca@gmail.com>
2019-10-17 11:51:05 -0300

    melhorando o projeto

--- java-io/src/br/com/alura/java/io/teste/TesteLeitura.java
+++ java-io/src/br/com/alura/java/io/teste/TesteLeitura.java
@@ -4,22 +4,28 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.Reader;
 
 public class TesteLeitura {
-	
+
 	public static void main(String[] args) throws IOException {
-		
-		//Fluxo de entrada com Arquivo
-		FileInputStream fis = new FileInputStream("lorem.txt"); // criando um fluxo concreto com dados binários
-		InputStreamReader isr = new InputStreamReader(fis); // melhorando os dados binários transformandoos em caracteres
+
+		// Fluxo de entrada com Arquivo
+		InputStream fis = new FileInputStream("lorem.txt"); // criando um fluxo concreto com dados binários
+		Reader isr = new InputStreamReader(fis); // melhorando os dados binários transformandoos em caracteres
 		BufferedReader br = new BufferedReader(isr); // juntando os caractertes em linhas
-		
+
 		String linha = br.readLine(); // lé uma linha
-		System.out.println(linha);
-		
+
+		while (linha != null) {
+			System.out.println(linha);
+			linha = br.readLine();
+		}
+
 		br.close();
-		
+
 	}
 
 }
