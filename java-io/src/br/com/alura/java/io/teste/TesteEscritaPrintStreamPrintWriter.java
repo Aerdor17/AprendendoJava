9770c60088d4b330a764368539ee2a6b63139467
Lucas <lebbeninca@gmail.com>
2019-10-22 11:43:54 -0300

    iniciando com arquivos .csv

--- java-io/src/br/com/alura/java/io/teste/TesteEscritaPrintStreamPrintWriter.java
+++ java-io/src/br/com/alura/java/io/teste/TesteEscritaPrintStreamPrintWriter.java
@@ -2,6 +2,7 @@ package br.com.alura.java.io.teste;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -12,10 +13,11 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintStream;
+import java.io.PrintWriter;
 import java.io.Reader;
 import java.io.Writer;
 
-public class TesteEscrita3 {
+public class TesteEscritaPrintStreamPrintWriter {
 
 	public static void main(String[] args) throws IOException {
 
@@ -24,9 +26,11 @@ public class TesteEscrita3 {
 			// OutputStream fos = new FileOutputStream("lorem2.txt"); // criando um fluxo concreto com dados binários
 			// Writer osw = new OutputStreamWriter(fos); // melhorando os dados binários transformandoos em caracteres
 			// BufferedWriter bw = new BufferedWriter(osw); // juntando os caractertes em linhas		
-			// bw.write("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod");
+			// bw.write("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod");		
+			//BufferedWriter bw = new BufferedWriter(new File("lorem2.txt")); // para um arquivo mais expecifico
+			//PrintWriter ps = new PrintWriter("lorem2.txt"); // metodo de mais alto nivel usando Writer fungindo da Stream
 		
-		PrintStream ps = new PrintStream("lorem2.txt");
+		PrintStream ps = new PrintStream("lorem2.txt"); 
 		
 		ps.println("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod");		
 		ps.println();
